d5e9f1c1ce27cfc25d5f14e1f75e47830c25e57e
Jean Menezes <jeanmenezes.nave@gmail.com>
2020-02-02 18:56:52 -0300

    Getting data from Star Wars API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    class App extends Component {
+      componentDidMount() {
+        fetch('https://swapi.co/api/planets/1/')
+        .then(res => res.json())
+        .then((data) => {
+          this.setState({ planet: data })
+        })
+        .catch(console.log)
+      }
 
-export default App;
+      render () {
+        return (
+          <div>{this.state ? this.state.planet.name : null}</div>
+        );
+      }
+    }
+
+    export default App;
