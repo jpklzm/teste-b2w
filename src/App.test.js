f5b0acc0fca64a28a397ba1282cc58d778fcccd9
Jean Menezes <jeanmenezes.nave@gmail.com>
2020-02-03 03:19:03 -0300

    Add some tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,33 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
+
+
+it("renders correctly", () => {
+  const wrapper = shallow(
+    <App />
+  );
+
+  expect(wrapper).toMatchSnapshot();
+});
+
+it("check lifecycle", () => {
+  const wrapper = mount(
+    <App />
+  );
+
+  expect(wrapper).toMatchSnapshot();
+});
+
+it("renders childrens correctly", () => {
+  const wrapper = render(
+    <App />
+  );
+
+  expect(wrapper).toMatchSnapshot();
 });
